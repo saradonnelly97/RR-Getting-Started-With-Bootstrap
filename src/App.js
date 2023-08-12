a71d729f6d1883a5a680f794453db3a82e81fc94
saradonnelly97 <sara.donnelly8@gmail.com>
2023-08-12 13:23:14 -0400

    imported bootstrap button successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import Button from './Components/Button';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Bootstrap Website</h1>
+      <h1>Bootstrap Website</h1>  
+      <Button />
      
     </div>
   );
---
+++ src/Components/Button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function Dark() {
+  return (
+    <>
+      <Button variant="outline-dark">Dark</Button>
+    </>
+  );
+}
+
+export default Dark;
